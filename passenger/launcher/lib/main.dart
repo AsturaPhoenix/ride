54231d0d97cfea4eb1c43e9dded552e05baf5ffc
Ross Wang <imagipioneer@gmail.com>
2023-11-26 09:41:38 -0800

    Analysis options
    Establish some common analysis configuration.

--- passenger/launcher/lib/main.dart
+++ passenger/launcher/lib/main.dart
@@ -8,17 +8,15 @@ class RideLauncher extends StatelessWidget {
   const RideLauncher({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'RIDE',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: WillPopScope(
-        onWillPop: () async => false,
-        child: const Scaffold(body: Text('home')),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'RIDE',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: WillPopScope(
+          onWillPop: () async => false,
+          child: const Scaffold(body: Text('home')),
+        ),
+      );
 }
